4ee52b6c0163c28993b606c46805c9e28526db12
Daniel Meurer <danielsmeurer@gmail.com>
2017-08-21 17:31:02 -0300

    adicionada pagina de erro 404

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php 
+$title="ERROR 404";
+include_once('template/header.php');?>
+<div class="container">
+	<div id="erro404" class="jumbotron">
+	<p>
+		<span style="font-size: 80px; letter-spacing:-10px; font-weight: bolder;">4
+		<i id='sad' class="fa fa-frown-o" aria-hidden="true"></i>
+		4</span>
+	</p>
+		<h2 >Pagina não encontrada</h2>
+	</div>
+</div>
+<?php include_once('template/footer.php');?>
