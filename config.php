12b5bcbbc1721227c97275e1715e77c6924e142a
Daniel Meurer <danielsmeurer@gmail.com>
2017-08-21 14:22:17 -0300

    criado arquivo de configuração do sistema

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Config
+*Arquivo de configuração básica do sistema.
+@author Daniel Meurer
+*/
+
+/*---------------------------------------------------------------------------------
+|Configuração do banco de dados|
+ ---------------------------------------------------------------------------------- */
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','sistema_reserva_salas');
+define('DB_PORT', 3306);
+
+/*---------------------------------------------------------------------------------
+|Configuração da url principal
+ ---------------------------------------------------------------------------------- */
+define("BASE_URL",'http://localhost/sistema_reserva_salas');
+define("FILES_BASE_ADDRESS",dirname( __FILE__ ) );			
+
+
+
+
+
